4dda65124545ac7bcc414e72846b0b91c7fdcbf2
Alexandru Paduraru <axelut@yahoo.com>
2025-10-22 14:59:57 +0300

    Initial commit

---
+++ apps/www/app/(app)/blocks/page.tsx
@@ -0,0 +1,18 @@
+import { BlockDisplay } from "@/components/block-display"
+
+export const dynamic = "force-static"
+export const revalidate = false
+
+const FEATURED_BLOCKS = [
+  "software-purchase-01",
+]
+
+export default async function BlocksPage() {
+  return (
+    <div className="flex flex-col gap-12 md:gap-24">
+      {FEATURED_BLOCKS.map((name) => (
+        <BlockDisplay name={name} key={name} />
+      ))}
+    </div>
+  )
+}
