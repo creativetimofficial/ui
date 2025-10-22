29e3c0cd1b20e38f7095a5fe490bb88280058354
dragosct10 <dragos@creative-tim.com>
2025-10-22 18:13:55 +0300

    Update layout.tsx

--- apps/www/app/(auth)/layout.tsx
+++ apps/www/app/(auth)/layout.tsx
@@ -1,8 +1,12 @@
+import { AuthProvider } from "@/components/auth/AuthProvider";
+
 // app/(auth)/layout.tsx
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
+    <AuthProvider>
     <div className="bg-background relative z-10 flex min-h-svh flex-col">
       <main className="flex flex-1 flex-col">{children}</main>
     </div>
+    </AuthProvider>
   );
 }
