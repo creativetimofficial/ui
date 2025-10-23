a7c44b0df27e505905cc4e1edb3184278d32d00a
dragosct10 <dragos@creative-tim.com>
2025-10-23 15:29:34 +0300

    Update route.ts

--- apps/www/app/api/auth/bootstrap/route.ts
+++ apps/www/app/api/auth/bootstrap/route.ts
@@ -4,7 +4,7 @@ export const runtime = "edge";
 
 export async function GET(req: Request) {
   // Always go to dashboard on success, login on failure
-  const successRedirect = new URL(`${process.env.NEXT_PUBLIC_APP_URL}/ui/dashboard`, req.url);
+  const successRedirect = new URL(`${process.env.NEXT_PUBLIC_APP_URL}/dashboard`, req.url);
   const loginRedirect = new URL(`${process.env.NEXT_PUBLIC_APP_URL}/ui/login`, req.url);
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_APP_URL}/api/v1/auth/refresh`, {
