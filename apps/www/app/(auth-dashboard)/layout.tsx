cfc63941c7ff998e4c9bb345c3b6ab9e2e746937
dragosct10 <dragos@creative-tim.com>
2025-10-31 12:38:59 +0200

    Update layout.tsx

--- apps/www/app/(auth-dashboard)/layout.tsx
+++ apps/www/app/(auth-dashboard)/layout.tsx
@@ -1,15 +1,10 @@
 import type { ReactNode } from "react";
-import DashboardShell from "@/components/auth/DashboardShell";
 import { AuthProvider } from "@/components/auth/AuthProvider";
-// import { SiteHeader } from "@/components/site-header";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <AuthProvider>
-        {/* <SiteHeader /> */}
-        <DashboardShell>{children}</DashboardShell>
-      </AuthProvider>
+      <AuthProvider>{children}</AuthProvider>
     </>
   );
 }
