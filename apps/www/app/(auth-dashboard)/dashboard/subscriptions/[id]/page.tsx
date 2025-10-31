2dbe915c63c062baa4eb801253be886bd02411d0
dragosct10 <dragos@creative-tim.com>
2025-10-31 12:25:21 +0200

    Update page.tsx

--- apps/www/app/(auth-dashboard)/dashboard/subscriptions/[id]/page.tsx
+++ apps/www/app/(auth-dashboard)/dashboard/subscriptions/[id]/page.tsx
@@ -1,6 +1,11 @@
+import DashboardShell from "@/components/auth/DashboardShell";
 import SubscriptionDetailClient from "./SubscriptionDetailClient";
 
 export default async function SubscriptionDetailPage({ params }: { params: { id: string } }) {
   const awaitedParams = await params;
-  return <SubscriptionDetailClient id={awaitedParams.id} />;
+  return (
+    <DashboardShell>
+      <SubscriptionDetailClient id={awaitedParams.id} />
+    </DashboardShell>
+  );
 }
