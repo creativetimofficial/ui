6578b4d0b5f33fd8631125315f46a4cf4292411c
dragosct10 <dragos@creative-tim.com>
2025-10-31 12:25:28 +0200

    Update page.tsx

--- apps/www/app/(auth-dashboard)/dashboard/payments/[id]/page.tsx
+++ apps/www/app/(auth-dashboard)/dashboard/payments/[id]/page.tsx
@@ -1,6 +1,11 @@
+import DashboardShell from "@/components/auth/DashboardShell";
 import PaymentsDetailClient from "./PaymentsDetailClient";
 
 export default async function PaymentDetailPage({ params }: { params: { id: string } }) {
   const awaitedParams = await params;
-  return <PaymentsDetailClient id={awaitedParams.id} />;
+  return (
+    <DashboardShell title="Payments">
+      <PaymentsDetailClient id={awaitedParams.id} />
+    </DashboardShell>
+  );
 }
